ca2788bd144fd9d8a95f53729512f4ccfe92500b
sebastian gonzalez trujillo <sebastiangonza1505@hotmail.com>
2023-09-21 23:55:17 -0500

    Creacion de layouts y pages

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="layout">
+    <v-img
+      max-height="150"
+      max-width="250"
+      padding="10%"
+      src="https://upload.wikimedia.org/wikipedia/commons/7/71/Marvel-Comics-Logo.svg"
+    ></v-img>
+  </div>
+  <slot />
+</template>
+<script setup>
+
+</script>
+<style>
+.layout {
+  background-color: black;
+  display: grid;
+  grid-template-columns: auto auto auto;
+/*   display: block; */
+  height: 10%;
+  width: auto;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Hola</h1>
+  </div>
+</template>
+<script setup>
+
+const router = useRouter()
+
+
+const user = ref({});
+
+definePageMeta({
+  layout: "default",
+});
+
+const consola = () => {
+  if (user.value.correo==="Sebas@gmail.com" && user.value.contraseña==="sebas123"){
+    router.push({
+      path: "/tareas"
+    })
+  }
+  else{
+    alert('Usuario incorrecto')
+  }
+
+};
+</script>
